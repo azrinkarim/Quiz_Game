c1b15b834f514aa3065fc085557ba5191a2b16f1
azrinkarim <159026844+azrinkarim@users.noreply.github.com>
2025-09-19 17:07:22 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import ContactPage from './pages/ContactPage/ContactPage'
+import AboutPage from './pages/Aboutpage/AboutPage'
+import Menu from './pages/Menu/Menu'
+import Cart from './pages/Cart/Cart';
+import SignUp from "./components/SignUp/SignUp";
+
+
+
+
+const App = () => {
+  return (
+    
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/contact' element={<ContactPage />}/>
+          <Route path='/about' element={<AboutPage />}/>
+          <Route path='/menu' element={<Menu />}/>
+          <Route path='/cart' element={<Cart />}/>
+          <Route path='/login' element={<Home />}/>
+          <Route path='/signup' element={<SignUp />}/>
+           
+      </Routes>
+    
+  )
+}
+
+export default App
